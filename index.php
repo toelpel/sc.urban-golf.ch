41d29ba328611109da1118cabbfaf2bf5a934beb
Christian Margadant <christian.margadant@digitecgalaxus.ch>
2025-04-01 14:02:44 +0200

    Beautified code :-)

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?phpinclude('includes/functions.php');// Datenbankverbindung aufbauen$GLOBALS['db_connect'] = db_connect();// Template einlesen$template = file_get_contents('style/template.html');$output = replace_html_vars($template);// Outputecho utf8_encode($output);//$sql = 'SELECT * FROM `sc_players` LIMIT 10';//$result = db_query($sql);//if ($result->num_rows) { printf("Select returned %d rows.\n", $result->num_rows); }?>
+<?php
+
+# Include functions
+include "includes/functions.php";
+
+// Setup database connection
+$GLOBALS["db_connect"] = db_connect();
+
+// Fetch template
+
+$template = file_get_contents("style/template.html");
+$output = replace_html_vars($template);
+
+// Output
+echo $output;
+
+?>
