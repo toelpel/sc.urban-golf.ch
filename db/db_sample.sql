f16f916b461c89cde1b003829c31cffbccf8fb21
Christian Margadant <christian.margadant@digitecgalaxus.ch>
2025-04-16 21:06:11 +0200

    Added DB Reset for test-environment

--- db/db_sample.sql
+++ db/db_sample.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 01, 2025 at 08:16 PM
--- Server version: 10.6.19-MariaDB-cll-lve-log
--- PHP Version: 8.1.32
+-- Erstellungszeit: 16. Apr 2025 um 20:56
+-- Server-Version: 10.6.19-MariaDB-cll-lve-log
+-- PHP-Version: 8.1.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,15 +18,16 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `teevau3c_wp1`
+-- Datenbank: `teevau3c_sctest`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sc_fg_games`
+-- Tabellenstruktur für Tabelle `sc_fg_games`
 --
 
+DROP TABLE IF EXISTS `sc_fg_games`;
 CREATE TABLE `sc_fg_games` (
   `id` int(8) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -34,24 +35,42 @@ CREATE TABLE `sc_fg_games` (
   `active` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Daten für Tabelle `sc_fg_games`
+--
+
+INSERT INTO `sc_fg_games` (`id`, `name`, `timestamp`, `active`) VALUES
+(467639, 'Testspiel 1', '2025-04-02 22:21:48', 1);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sc_fg_players`
+-- Tabellenstruktur für Tabelle `sc_fg_players`
 --
 
+DROP TABLE IF EXISTS `sc_fg_players`;
 CREATE TABLE `sc_fg_players` (
   `id` int(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `gameid` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Daten für Tabelle `sc_fg_players`
+--
+
+INSERT INTO `sc_fg_players` (`id`, `name`, `gameid`) VALUES
+(1, 'Spieler 1', 467639),
+(2, 'Spieler 2', 467639),
+(3, 'Spieler 3', 467639);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sc_fg_results`
+-- Tabellenstruktur für Tabelle `sc_fg_results`
 --
 
+DROP TABLE IF EXISTS `sc_fg_results`;
 CREATE TABLE `sc_fg_results` (
   `id` int(255) NOT NULL,
   `gameid` int(255) NOT NULL,
@@ -61,42 +80,63 @@ CREATE TABLE `sc_fg_results` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Indexes for dumped tables
+-- Daten für Tabelle `sc_fg_results`
+--
+
+INSERT INTO `sc_fg_results` (`id`, `gameid`, `holeid`, `playerid`, `result`) VALUES
+(1, 467639, 1, 1, 3),
+(2, 467639, 1, 2, 5),
+(3, 467639, 1, 3, 4),
+(4, 467639, 2, 1, 6),
+(5, 467639, 2, 2, 4),
+(6, 467639, 2, 3, 7),
+(7, 467639, 3, 1, 1),
+(8, 467639, 3, 2, 4),
+(9, 467639, 3, 3, 2),
+(10, 467639, 4, 1, 4),
+(11, 467639, 4, 2, 8),
+(12, 467639, 4, 3, 11),
+(13, 467639, 5, 1, 5),
+(14, 467639, 5, 2, 2),
+(15, 467639, 5, 3, 3);
+
+--
+-- Indizes der exportierten Tabellen
 --
 
 --
--- Indexes for table `sc_fg_games`
+-- Indizes für die Tabelle `sc_fg_games`
 --
 ALTER TABLE `sc_fg_games`
   ADD UNIQUE KEY `id` (`id`);
 
 --
--- Indexes for table `sc_fg_players`
+-- Indizes für die Tabelle `sc_fg_players`
 --
 ALTER TABLE `sc_fg_players`
   ADD UNIQUE KEY `id` (`id`);
 
 --
--- Indexes for table `sc_fg_results`
+-- Indizes für die Tabelle `sc_fg_results`
 --
 ALTER TABLE `sc_fg_results`
   ADD UNIQUE KEY `id` (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT für exportierte Tabellen
 --
 
 --
--- AUTO_INCREMENT for table `sc_fg_players`
+-- AUTO_INCREMENT für Tabelle `sc_fg_players`
 --
 ALTER TABLE `sc_fg_players`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `sc_fg_results`
+-- AUTO_INCREMENT für Tabelle `sc_fg_results`
 --
 ALTER TABLE `sc_fg_results`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
